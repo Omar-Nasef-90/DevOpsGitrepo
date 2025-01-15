2aab7345117c660c34562fd73d51b1286c4e1287
Omar-Nasef-90 <omarnasef909@gmail.com>
2025-01-15 18:27:29 +0200

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,19 @@
+def buildJar() {
+    echo "building the application..."
+    sh 'mvn package'
+} 
+
+def buildImage() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t omarnasef/demo-app-for-maven:jma-1.0 .'
+        sh "docker login -u $USERNAME -p $PASSWORD"
+        sh 'docker push omarnasef/demo-app-for-maven:jma-1.0'
+    }
+} 
+
+def deployApp() {
+    echo 'deploying the application...'
+} 
+
+return this
